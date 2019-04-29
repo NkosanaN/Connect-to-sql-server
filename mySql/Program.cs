89a75503f545ed9f6a751ed501b49b4129eb4314
Nkosana Ndlela <nkosana@datasaint.com>
2019-04-29 12:12:10 +0200

    First commit

---
+++ mySql/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace mySql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = @"TRAINING-LT7\SQLEXPRESS";
+                builder.UserID = "sa";
+                builder.Password = "secret@2011";
+                builder.InitialCatalog = "PO_SO_stock_control_system";
+                // Connect to SQL
+                Console.Write("Connecting to SQL Server ... ");
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    connection.Open();
+                    Console.WriteLine("Done.");
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("All done. Press any key to finish...");
+            Console.ReadKey(true);
+
+        }
+    }
+}
